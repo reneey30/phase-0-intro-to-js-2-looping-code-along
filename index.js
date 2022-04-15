b4ddf0b874421b201c90b6cf640ae6bd948f541e
Irene Njeru <irenenjeru8@gmail.com>
2022-04-15 12:34:55 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(no){
+  let a = 0;
+  while(a < no + 1){
+      console.log(no - a)
+      a++
+  }
+}
+
+function writeCards(arr, msg){
+  let cardsMsg = [];
+  for(let item in arr){
+      cardsMsg.push(`Thank you, ${arr[item]}, for the wonderful ${msg} gift!`);
+  }
+  return cardsMsg;
+}
